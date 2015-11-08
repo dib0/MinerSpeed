76e51001c64f3ef761d13331463327b03fb5c845
Bas van den Berg <bas@dib0.nl>
2015-11-08 17:25:24 +0100

    Screen setup

--- src/main.c
+++ src/main.c
@@ -5,8 +5,14 @@ TextLayer *text_layer;
 
 void handle_init(void) {
   my_window = window_create();
+  
+  // Window setup
+  window_set_background_color(my_window, GColorFromRGB(85, 85, 85));
 
   text_layer = text_layer_create(GRect(20, 50, 100, 20));
+  //text_layer_set_text_color(text_layer, GColorFromRGB(85, 85, 85));
+  text_layer_set_text_color(text_layer, GColorFromRGB(0, 0, 210));
+  
   text_layer_set_text(text_layer, "Miner stats:");
   
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_layer));
